918e01dd9768c1785b13ede4afbef502beebd245
Edwardscb <39107183+Edwardscb@users.noreply.github.com>
2022-06-26 12:16:59 -0400

    Update fooBar.js

--- fooBar.js
+++ fooBar.js
@@ -8,6 +8,12 @@
 
 function fooBar(num) {
 
+    // use the modulo character to determine if something is divisible by another number
+    // for example; You can check if something is even or not by using % 2 === 0.
+    // here we use modulo 3 and modulo 5 to check for 3 and 5 specifically.
+    
+    // additionally, the first line of code must be this one, otherwise the function will not
+    // work properly due to how the boolean operators determine what will be evaluated
     if (num % 3 === 0 && num % 5 === 0) {
         return "foo bar";
     } else if (num % 3 === 0) {
